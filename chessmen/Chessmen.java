c8f23bd855592c4b92a756d8b283f8be3d39a22a
Yuhiro <powerofyuhiro@gmail.com>
2018-05-03 16:58:03 +0200

    todo bien

---
+++ chessmen/Chessmen.java
@@ -0,0 +1,55 @@
+package chessmen;
+
+import java.util.ArrayList;
+
+public class Chessmen {
+	
+	protected char color;
+	protected String position;
+	protected String name;
+	
+	protected Exception InvalidPosition;
+	protected Exception InvalidColor;
+	
+	public Chessmen (String position, char color) throws Exception {
+		if (correctColor(color)) {
+			this.position = position;
+			this.color = color;
+		} else {
+			throw InvalidColor;
+		}
+	}
+	
+	public String getPosition() {
+		return position;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public char getColor() {
+		return color;
+	}
+	
+	public void moveTo(String new_position) throws Exception {
+		ArrayList<String> posible = this.previewMovement();
+		
+		if (posible.indexOf(new_position) >= 0) {
+			this.position = new_position;
+		} else {
+			throw InvalidPosition;
+		}
+	}
+	
+	public ArrayList<String> previewMovement() {
+		return new ArrayList<String>();
+	}	
+
+	private boolean correctColor(char color) {
+		if (color == 'w' || color == 'b') {
+			return true;
+		}
+		return false;
+	}
+}
