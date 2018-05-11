16225b6b2867953ab186c7bbd08d4a62501a151e
marcfs31 <marcfs31@protonmail.com>
2018-05-11 18:48:26 +0200

    All done

--- chessmen/Pawn.java
+++ chessmen/Pawn.java
@@ -1,3 +1,4 @@
+//Marc Fors
 package chessmen;
 
 import java.util.ArrayList;
@@ -5,17 +6,19 @@ import java.util.Arrays;
 import board.ChessBoard;
 
 public class Pawn extends Chessmen{
+	
+	//Constructor
 	public Pawn (String position, char color) throws Exception {
 		super(position, color);
 		name = "P" + 	color;
 	}
 	
-
+	//Methods
 	public ArrayList<String> previewMovement() {
-		int[][] posible = {{0,2},{0,1},{1,1},{-1,1}};
+		int[][] posible = {{0,2},{0,1},{1,1},{-1,1}}; //Posible movements for White chessmen
 		
-		if (color == 'b') {			
-			for (int i = 0; i < posible.length; i++) {
+		if (color == 'b') {
+			for (int i = 0; i < posible.length; i++) { //Change the values of posible to match the posible movements for black chessmen
 				posible[i][1] = -posible[i][1];
 				System.out.println(posible[i][1]);
 			}
@@ -26,13 +29,10 @@ public class Pawn extends Chessmen{
 		char[] alphaRow = ChessBoard.getAlphaRow();
 		int[] actualPosition = {Arrays.binarySearch(numRow, this.position.charAt(1)),Arrays.binarySearch(alphaRow, this.position.charAt(0))};
 		
-		for (int i = 0; i < posible.length; i++) {
+		for (int i = 0; i < posible.length; i++) { //HUlio
 			try {
 				positions.add(alphaRow[actualPosition[1]+posible[i][0]] + "" + numRow[actualPosition[0]+posible[i][1]]);
 			} catch (Exception e) {}
-		}
-		for (int i = 0; i < positions.size(); i++) {
-			System.out.println(positions.get(i));
 		}
 		return positions;
 	}
